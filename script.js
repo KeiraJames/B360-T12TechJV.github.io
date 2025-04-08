fa9f667cd79015fa04da7f06e39328796c087d10
Keira James <121837616+KeiraJames@users.noreply.github.com>
2025-04-08 12:29:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const background = new Image();
+background.src = "images/background.jpg";
+background.onload = () => {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  const img1 = new Image();
+  img1.src = "images/image1.png";
+  img1.onload = () => {
+    ctx.drawImage(img1, 100, 300, 200, 250);
+
+    const img2 = new Image();
+    img2.src = "images/image2.png";
+    img2.onload = () => {
+      ctx.drawImage(img2, 500, 250, 150, 150);
+
+      ctx.font = "28px Arial";
+      ctx.fillStyle = "white";
+      ctx.fillText("Keira - My Canvas Scene", 20, 50);
+    };
+  };
+};
 
